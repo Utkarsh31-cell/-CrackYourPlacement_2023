ac77437ec1e27f3bce92d58bee4df4072c8efeb0
Utkarsh31-cell <78687661+Utkarsh31-cell@users.noreply.github.com>
2023-07-06 16:39:38 +0530

    Update code4.cpp

--- Day1/code4.cpp
+++ Day1/code4.cpp
@@ -1,1 +1,56 @@
+//problem link -> https://practice.geeksforgeeks.org/problems/chocolate-distribution-problem3825/1
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
 
+// } Driver Code Ends
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+    //code
+        sort(a.begin(),a.end());
+        long long int mini=INT_MAX;
+        for(int i=0;i<=n-m;i++){
+            
+           long long int cnt=a[i+m-1]-a[i];
+          // cout<<cnt<<" ";
+            mini=min(mini,cnt);
+            
+        }
+        return mini;
+    
+    }   
+};
+
+//{ Driver Code Starts.
+int main() {
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		vector<long long> a;
+		long long x;
+		for(long long i=0;i<n;i++)
+		{
+			cin>>x;
+			a.push_back(x);
+		}
+		
+		long long m;
+		cin>>m;
+		Solution ob;
+		cout<<ob.findMinDiff(a,n,m)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
+/*
+1
+6
+11 13 7 5 13 12
+4
+output:
+2
+*/
