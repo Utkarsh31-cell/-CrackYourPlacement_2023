3e90687bfeacecbf15f4d3a088103916ee884770
Utkarsh31-cell <78687661+Utkarsh31-cell@users.noreply.github.com>
2023-07-06 20:13:33 +0530

    Update code6.cpp

--- Day1/code6.cpp
+++ Day1/code6.cpp
@@ -1,1 +1,21 @@
+//problem link:->https://leetcode.com/problems/sort-colors/submissions/987851315/
 
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low=0,mid=0,high=nums.size()-1;
+        //Dutch National Flag problem
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[mid],nums[low]);
+                low++;
+                mid++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
+    }
+};
