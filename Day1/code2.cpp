6eafe7053b28d7e59810d2175ba9064c92408dd1
Utkarsh31-cell <78687661+Utkarsh31-cell@users.noreply.github.com>
2023-07-06 14:57:23 +0530

    Update code2.cpp

--- Day1/code2.cpp
+++ Day1/code2.cpp
@@ -1,1 +1,24 @@
+//problem link-> https://leetcode.com/problems/move-zeroes/submissions/987653099/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int cnt=0;
+        vector<int>v;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                cnt+=1;
+            }else{
+                v.push_back(nums[i]);
+            }
+        }
+        for(int i=0;i<nums.size();i++){
+            if(i<v.size()){
+                nums[i]=v[i];
+            }else{
+                nums[i]=0;
+            }
+        }
+    }
+};
 
