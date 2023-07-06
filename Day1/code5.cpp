15711f8fe0b5a51a54899c3d0b4fa03b59b3137e
Utkarsh31-cell <78687661+Utkarsh31-cell@users.noreply.github.com>
2023-07-06 18:34:26 +0530

    Update code5.cpp

--- Day1/code5.cpp
+++ Day1/code5.cpp
@@ -1,1 +1,20 @@
+//problem:->https://leetcode.com/problems/find-the-duplicate-number/submissions/799127147/
 
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int i=0,j=1;
+        int ans=0;
+        sort(nums.begin(),nums.end());
+        while(i<=j){
+            if(nums[i]==nums[j]){
+                ans=nums[i];
+                 break;
+            }else{
+               i++;
+               j++;
+            }
+        }
+        return ans;
+    }
+};
