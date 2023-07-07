960575bac376f27684961d31a3f3f9e09e4540e4
Utkarsh31-cell <78687661+Utkarsh31-cell@users.noreply.github.com>
2023-07-07 17:05:44 +0530

    Update code6.cpp

--- Day2/code6.cpp
+++ Day2/code6.cpp
@@ -1,1 +1,23 @@
+//Problem Link ->https://leetcode.com/problems/container-with-most-water/submissions/988532169/
 
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i=0,n=height.size();
+        int j=n-1;
+        int sum=0;
+        int maxi=0;
+        while(i<j){
+            sum=min(height[j],height[i]);
+            maxi=max(maxi,(j-i)*sum);
+          //first find water in area as min(height of two side of container * distance between them is area)
+          //after this index updation done according to ith height of container to jth container height and update index of minimum height container
+            if(height[j]>height[i]){           
+                i++;
+            }else{
+                j--;
+            }
+        }
+        return maxi;
+    }
+};
