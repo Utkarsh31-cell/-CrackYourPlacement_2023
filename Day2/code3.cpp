91e51e35d55247ceb8ca6b911b239edbbddb4510
Utkarsh31-cell <78687661+Utkarsh31-cell@users.noreply.github.com>
2023-07-07 15:23:59 +0530

    Update code3.cpp

--- Day2/code3.cpp
+++ Day2/code3.cpp
@@ -1,1 +1,23 @@
+//problem link :-> https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/submissions/988477814/
 
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxi=0,cnt=0;
+        vector<int>v;
+        int n=prices.size();
+       // maxi=prices[0];
+        for(int i=1;i<n;i++){
+           // maxi=max(maxi,prices[i]);
+          v.push_back(prices[i]-prices[i-1]);
+        }
+        //since we hold stock hence we have to find max each time not in maximum subarray
+        for(int i=0;i<v.size();i++){
+            if(cnt+v[i]>=cnt){
+                cnt+=v[i];
+            }
+            maxi=max(maxi,cnt);
+        }
+        return maxi;
+    }
+};
