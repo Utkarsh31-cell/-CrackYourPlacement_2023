08ab5364e778e4105049230affbc9ceeb7e2eb4a
Utkarsh31-cell <78687661+Utkarsh31-cell@users.noreply.github.com>
2023-07-15 14:15:27 +0530

    Update code2.cpp

--- Day4/code2.cpp
+++ Day4/code2.cpp
@@ -1,1 +1,37 @@
 //problem link -> https://leetcode.com/problems/merge-sorted-array/
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=0,j=0;
+        
+        vector<int>newa;
+        while((i<m) || (j<n)){
+            if((i<m)&&(j<n)){
+               
+             if(nums1[i]<nums2[j]){
+               //    cout<<nums1[i]<<" ";
+                 newa.push_back(nums1[i]);
+                 i++;
+             }else{
+                 //   cout<<nums2[j]<<" ";
+                 newa.push_back(nums2[j]);
+                 j++;
+             }
+            }else if((i<m)&&(j>=n)){
+                 //  cout<<nums1[i]<<" ";
+                newa.push_back(nums1[i]);
+            i+=1;
+            }else if((j<n)&&(i>=m)){
+               //  cout<<nums2[j]<<" ";
+                  newa.push_back(nums2[j]);
+                j+=1;
+            }
+        }
+        nums1.clear();
+       int f=newa.size();
+        cout<<f<<" ";
+        for(i=0;i<f;i++){
+         nums1.push_back(newa[i]);     
+        }
+    }
+};
