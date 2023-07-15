cfa59fcdeccb62f549adf320b2fcccedf6833c8f
Utkarsh31-cell <78687661+Utkarsh31-cell@users.noreply.github.com>
2023-07-15 14:28:39 +0530

    Update code3.cpp

--- Day4/code3.cpp
+++ Day4/code3.cpp
@@ -1,1 +1,21 @@
 //problem lnk ->https://leetcode.com/problems/majority-element/
+/*
+here we have to find element of the array whose frequency is more than n/2  :->frequency means count of that element
+*/
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int,int>mp;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
+        }
+        int count=0;
+        for(auto it : mp){
+            if(it.second>(n/2)){
+                return it.first;
+            }
+        }
+        return count;
+    }
+};
